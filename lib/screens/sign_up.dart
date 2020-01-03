57c6562187b9351721cd0ad0f3f2f6ec5bcadbc9
realsst1 <s@Nuchotu1>
2020-01-03 21:02:34 +0530

    added firebase

--- lib/screens/sign_up.dart
+++ lib/screens/sign_up.dart
@@ -28,91 +28,93 @@ class _SignupScreenState extends State<SignupScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Instagram",
-              style: TextStyle(
-                  fontSize: 50.0,
-                  fontFamily: 'Billabong'
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Instagram",
+                style: TextStyle(
+                    fontSize: 50.0,
+                    fontFamily: 'Billabong'
+                ),
               ),
-            ),
-            Form(
-              key: _formKey,
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal:30.0,vertical: 10.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Name',
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal:30.0,vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Name',
+                        ),
+                        validator: (input)=>input.isEmpty?'Please enter valid name':null,
+                        onSaved: (input)=>_name=input,
                       ),
-                      validator: (input)=>input.isEmpty?'Please enter valid name':null,
-                      onSaved: (input)=>_name=input,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal:30.0,vertical: 10.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Email',
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal:30.0,vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                        ),
+                        validator: (input)=>!input.contains('@')?'Please enter valid email':null,
+                        onSaved: (input)=>_email=input,
                       ),
-                      validator: (input)=>!input.contains('@')?'Please enter valid email':null,
-                      onSaved: (input)=>_email=input,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal:30.0,vertical: 10.0),
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelText: 'Password',
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal:30.0,vertical: 10.0),
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'Password',
+                        ),
+                        validator: (input)=>input.length<6?'Password must be atleast 6 characters':null,
+                        onSaved: (input)=>_password=input,
+                        obscureText: true,
                       ),
-                      validator: (input)=>input.length<6?'Password must be atleast 6 characters':null,
-                      onSaved: (input)=>_password=input,
-                      obscureText: true,
                     ),
-                  ),
-                  SizedBox(height: 20.0,),
-                  Container(
-                    width: 300.0,
-                    child: FlatButton(
-                      onPressed: _submit,
-                      padding: EdgeInsets.all(14.0),
-                      color: Colors.blue,
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16.0
+                    SizedBox(height: 20.0,),
+                    Container(
+                      width: 300.0,
+                      child: FlatButton(
+                        onPressed: _submit,
+                        padding: EdgeInsets.all(14.0),
+                        color: Colors.blue,
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 20.0,),
-                  Container(
-                    width: 300.0,
-                    child: FlatButton(
-                      onPressed: ()=>Navigator.pop(context),
-                      padding: EdgeInsets.all(14.0),
-                      color: Colors.blue,
-                      child: Text(
-                        'Already Registered? Sign In',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 16.0
+                    SizedBox(height: 20.0,),
+                    Container(
+                      width: 300.0,
+                      child: FlatButton(
+                        onPressed: ()=>Navigator.pop(context),
+                        padding: EdgeInsets.all(14.0),
+                        color: Colors.blue,
+                        child: Text(
+                          'Already Registered? Sign In',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0
+                          ),
                         ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            )
-          ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
